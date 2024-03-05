278b38b79685eee53b622417456e34d2e284df5e
aihassan1 <abdosruor57@gmail.com>
2024-03-05 13:22:13 +0200

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and returns the number of subscribers"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """function that queries the Reddit API and
+    returns the number of subscribers
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    # Provide a custom User-Agent header
+    headers = {"User-Agent": "custom_user/1.0"}
+
+    response = requests.get(url=url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
