20c258010d6346599f44ca921f69435187638297
aihassan1 <abdosruor57@gmail.com>
2024-03-05 13:23:16 +0200

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and returns the number of subscribers"""
+"""function that queries the Reddit API and
+    returns the number of subscribers
+"""
+
+
 import requests
+
 
 def number_of_subscribers(subreddit):
     """function that queries the Reddit API and
