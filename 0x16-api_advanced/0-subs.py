15cc9115dcff41b35d31c8823d0baedddb7c29de
aihassan1 <abdosruor57@gmail.com>
2024-03-06 13:30:05 +0200

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,13 +10,4 @@ def number_of_subscribers(subreddit):
     """function that queries the Reddit API and
     returns the number of subscribers
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    # Provide a custom User-Agent header
-    headers = {"User-Agent": "custom_user_1.0"}
-
-    response = requests.get(url=url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-
-    data = response.json()
-    return data["data"]["subscribers"]
+    pass
