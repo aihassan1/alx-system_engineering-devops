69aa386b5e73424319aae809664d73a33ce445d5
aihassan1 <abdosruor57@gmail.com>
2024-03-06 15:10:33 +0200

    asd

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,22 @@
-    #!/usr/bin/python3
+#!/usr/bin/python3
+"""function that queries the Reddit API and
+    returns the number of subscribers
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
     """function that queries the Reddit API and
-        returns the number of subscribers
+    returns the number of subscribers
     """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    # Provide a custom User-Agent header
+    headers = {'User-Agent': 'Reddit API Client'}
 
-    import requests
-
-
-    def number_of_subscribers(subreddit):
-        """function that queries the Reddit API and
-        returns the number of subscribers
-        """
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        # Provide a custom User-Agent header
-        headers = {'User-Agent': 'Reddit API Client'}
-
-        response = requests.get(url=url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data["data"]["subscribers"]
-        else:
-            return 0
+    response = requests.get(url=url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
