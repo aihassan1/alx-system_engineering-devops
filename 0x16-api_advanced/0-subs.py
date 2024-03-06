aa4acbe46625b6618b0b699f3cdadc43aedcc8b4
aihassan1 <abdosruor57@gmail.com>
2024-03-06 15:09:07 +0200

    asd

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     # Provide a custom User-Agent header
-    headers = {"User-Agent": "Reddit API Client"}
+    headers = {'User-Agent': 'Reddit API Client'}
 
     response = requests.get(url=url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
