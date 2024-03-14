e7fb269dc791f1150e1a651f6827ad6f07cca544
aihassan1 <abdosruor57@gmail.com>
2024-03-14 14:45:23 +0200

    0-the_sky_is_the_limit_not.pp11

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,20 +1,15 @@
 # nginx_config.pp
 
 # Define an exec resource to replace the line that starts with 'worker_processes' using sed
-exec { 'set_worker_processes':
-  command  => "sed -i 's/[[:space:]]*worker_processes [0-9]*;/worker_processes 14;/' /etc/nginx/nginx.conf",
+exec { 'change_ulimit':
+  command  => "sed -i 's/ULIMIT=\"-n [0-9]*\"/ULIMIT=\"-n 4096\"/' /etc/default/nginx",
   provider => shell,
 }
 
-# Define an exec resource to uncomment the 'multi_accept' directive using sed
-exec { 'enable_multi_accept':
-  command  => "sed -i 's/[[:space:]]*#\\s*multi_accept\\s*on;/multi_accept on;/' /etc/nginx/nginx.conf",
-  provider => shell,
-}
 
 # Define an exec resource to restart the Nginx service
 exec { 'restart_nginx':
   command  => 'service nginx restart',
   provider => shell,
-  require  => [Exec['set_worker_processes'], Exec['enable_multi_accept']],
+  require  => [Exec['change_ulimit']],
 }
